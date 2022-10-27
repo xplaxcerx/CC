c42b8816c32836fb3e7a058b52c3916da9df4e88
xplaxcerx <egorov11012005@mail.ru>
2022-10-27 19:02:02 +0500

    laba

--- lb3.c
+++ lb3.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
-int main() {
-    
+#include <math.h>
+int main () {
+    double x = 0, h, f, i, n;
+    printf ("Enter h (step) -> ");
+    scanf ("%lf", &h);
+    printf("\nx             f(x)\n- - - - - - - - - - - -\n");
+    n = 1.5 / h;
+    for (i = 0; i <= n; i++)
+    {
+        if (i <= (n / 2))
+            f = ((pow(x, 2)-pow(2*x, 3))*cos(pow(x,2)));
+        else
+            f = exp(sin(x));
+        printf("%lf      %lf\n", x, f);
+        x += h;
+    }
+    return 0;
 }
