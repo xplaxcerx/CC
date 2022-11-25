1cd340668ca6fccacdb0b218e49a8c8093a420c5
xplaxcerx <115776678+xplaxcerx@users.noreply.github.com>
2022-11-25 11:18:25 +0500

    Update 6.c

--- 6.c
+++ 6.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-unsigned long long int Nod(unsigned long long int a, unsigned long long int b)
+unsigned long long int nod (unsigned long long int a, unsigned long long int b)
 {
     while (a && b)
         if (a >= b)
@@ -16,30 +16,19 @@ void mass (unsigned long long int n, unsigned long long int a[])
     for (i = 0; i < n; i++)
         a[i] = rand ();
 }
-unsigned long long int u (unsigned long long int a, unsigned long long int b)
 
-    // scanf("%d", &a);
-    // scanf("%d",&b);
-    // if (Nod(a,b)==1){
+unsigned long long int(
 
-    //     printf("Числа взаимно простые\n");
-    // }
-    // else 
-    // {
-    //     printf("Числа НЕ взаимно простые\n");
-    // }
-    // printf("%d\n", Nod(a,b));
 int main()
 {   
     srand(time(NULL));
     int n;
     printf("n -> ");
     scanf("%d", &n);
-    int A[n];
-    fill(n, A);
+    int a[n];
+    fill(n, a);
     int i;
     for (i = 0; i < n; i++)
-    printf("%d", A[i]);
 
 
     // int a,b;
